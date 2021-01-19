bcf1e65399214ff1b9c043be69e19c5956a7d0e5
Mike Meloy <mike.meloy@centralreach.com>
2021-01-19 08:02:33 -0600

    messing around with camera api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const videoStream = navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      const video = document.querySelector("#myVideo");
+      video.srcObject = stream;
+    });
+  const handler = () => {
+    const canvas = document.createElement("canvas"),
+      video = document.querySelector("#myVideo"),
+      img = document.createElement("img"),
+      screenshotsContainer = document.getElementById("screenshotsContainer");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+
+    //maybe save all?
+    Array.from(screenshotsContainer.children).forEach((child) => {
+      screenshotsContainer.removeChild(child);
+    });
+
+    img.src = canvas.toDataURL("image/png");
+    screenshotsContainer.appendChild(img);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <video id="myVideo" autoPlay></video>
+      <div className="player">
+        <input type="button" value="Snap" onClick={handler} />
+      </div>
+      <div id="screenshotsContainer"></div>
+    </>
   );
 }
 
